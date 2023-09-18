c96097be4baed730e0544fb85b08c2c42868dcfc
doraemon0807 <98371903+doraemon0807@users.noreply.github.com>
2023-09-17 20:23:26 -0400

    AppLoading

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    //prepare all the pre-loads
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        const fontsToLoad = [Ionicons.font];
+        const fontPromises = fontsToLoad.map(
+          async (font) => await Font.loadAsync(font)
+        );
+
+        // Pre-Load images
+        const imagesToLoad = [require("./assets/logo.png")];
+        const imagePromises = imagesToLoad.map((image) =>
+          Asset.loadAsync(image)
+        );
+
+        Promise.all([...fontPromises, ...imagePromises]);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (appIsReady) {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
